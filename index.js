88cded7291c20c3fde75e1287bebb66dbcb3b9f0
vigneshcodesfyi <vigneshcodesfyi@gmail.com>
2024-08-02 18:34:20 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,54 @@
+// const back1 = document.getElementById('container1')
+// const back2 = document.getElementById('container2')
+// const back3 = document.getElementById('container3')
+
+
+// back1.addEventListener('click',changebackground1)
+// back2.addEventListener('click',changebackground2)
+// back3.addEventListener('click',changebackground3)
+// function changebackground1(){
+
+// const back = document.getElementById('container');
+// back.style.backgroundImage = "url('46poLC.jpg')";
+
+// back.style.zIndex=1;
+// }
+// function changebackground2(){
+// const back = document.getElementById('container');
+// back.style.backgroundImage = "url('538172.jpg')";
+// back.style.zIndex=1;
+// }
+// function changebackground3(){
+
+// const back = document.getElementById('container');
+// back.style.backgroundImage = "url('AqfxEd.jpg')";
+// back.style.zIndex=1;
+// }
+// const card = document.getElementById('card')
+// const show = document.getElementById('about');
+//  const details = document.getElementById('product');
+//  const productinfo = document.getElementById('productinfo');
+
+// card.addEventListener('mouseover',showcontents)
+// card.addEventListener('mouseout',hidecontents)
+
+// function showcontents(){
+
+// show.style.display="block"
+// card.style.width='40%';
+// product.style.width='90%';
+
+// product.style.justifyContent="space-around";
+// productinfo.style.textAlign="center"
+
+// }
+
+// function hidecontents(){
+   
+   
+//    show.style.display="none"
+  
+//    product.style.width='200px';
+//    card.style.width='max-content';
+
+//    }
